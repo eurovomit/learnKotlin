f003d17caa45d7bedbd301647b6cd10f68ea0de8
eurovomit <eurovomit@gmail.com>
2025-01-04 12:02:46 +0200

    10 коммит

--- src/learn/012_коллекции.kt
+++ src/learn/012_коллекции.kt
@@ -53,6 +53,7 @@ fun main() {
     println("уникальный список $lst5")
 
 
+
     //    ==============МНОЖЕСТВА=============
 //    добавление и удаление аналогично спискам
 
@@ -70,5 +71,54 @@ fun main() {
 
 //случайное значение из множества
     println(set1.random())
+
+
+    //    ==============СЛОВАРИ=============
+    
+//    создать словарь
+    val dict1 = mapOf("a" to 1, "b" to 2, "c" to 3)
+    val dict2 = mapOf(Pair("a", 1), Pair("b", 2), Pair("c", 3))
+    val dict3 = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+
+//    обратиться к значениям
+    println(dict1["b"])                                                     // если нет ключа - вернет null
+    println(dict1.getValue("a"))                                        // если нет ключа - вернет исключение
+    println(dict1.getOrDefault("d", 0))                     // если нет ключа - вернет default
+
+//    добавить значение
+    dict3 += "d" to 4
+    dict3 += mapOf("e" to 5, "f" to 6)
+
+//    поменять значение по ключу или добавить значение
+    dict3["f"] = 7
+    dict3.put("f", 6)
+    dict3.putAll(listOf("g" to 7, "h" to 8))
+    dict3.getOrPut("k"){9} // если такого ключа нет - возвращается новое значение, если ключ есть - старое значение
+
+//      удалить значения
+    dict3.remove("k")
+    dict3 -= listOf("a", "b")
+    dict3.clear()
+
+
+    //    пример преобразования списков в словарь попроще
+    val lst7 = listOf("z", "y", "w")
+    val lst8 = listOf(11, 12, 13)
+    val dict5 = List(3){index -> lst7[index] to lst8[index]}.toMap()
+    println(dict5)
+
+//    пример преобразования списков в словарь посложнее
+    val lst6 = listOf("a_1", "c_2", "e_3")
+    val dict4: Map<String, Int> =
+        List(3){index ->
+                            val (z, y) = lst6[index].split("_")
+                            z to y.toInt()}.toMap()
+    println(dict4)
+
+
+//    перебор элементов словаря
+    val dict6 = mutableMapOf("a" to 1.087657654, "b" to 2.0765476546, "c" to 3.987676565476)
+    dict6.forEach{(str, value) -> println("$str=${"%.2f".format(value)}")}
+    dict6.forEach{ println("${it.key}=${it.value}") }
 
 }
