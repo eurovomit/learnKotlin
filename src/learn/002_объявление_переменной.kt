84c0dbf5e73007c8362a22a1df82a561568002d8
Alexey <eurovomit@gmail.com>
2026-03-14 09:23:37 +0200

    add var and val

--- src/learn/002_объявление_переменной.kt
+++ src/learn/002_объявление_переменной.kt
@@ -1,20 +1,9 @@
 package learn
 
-const val COMPILE: Int = 8 // константа времени компиляции
 
-fun main() {
-    // изменяемая переменная
-    var playerLevel: Int = 2
-    println(playerLevel)
-    playerLevel++
-    println(playerLevel)
+//изменяемая переменная
+var a = 1
 
-    //    неизменяемая переменная
-    val playerName: String = "Vika"
-    println(playerName)
 
-    // автоматическре определение типов
-    val int = 5
-    val string = "String"
-
-}
+//неизменяемая переменная
+val b = 2
